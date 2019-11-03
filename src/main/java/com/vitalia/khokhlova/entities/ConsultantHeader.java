2909b5702d86579c8bc39b821dc6e628e1b76614
vitaliakhokhlova <eternele@gmail.com>
2019-11-03 17:01:36 +0100

    post and put added

---
+++ src/main/java/com/vitalia/khokhlova/entities/ConsultantHeader.java
@@ -0,0 +1,106 @@
+package com.vitalia.khokhlova.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
+
+@MappedSuperclass
+public class ConsultantHeader  extends GenericEntity {
+	
+	@Column(nullable=false, length=50)
+	private String firstname;	
+	@Column(nullable=false, length=50)
+	private String lastname;	
+	private String title;
+	private String profile;
+	private String expression;
+	private String author;
+	private String interests;
+	private String occupancy;
+	private String mobility;	
+	private Date birthday;
+	
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	public String getExpression() {
+		return expression;
+	}
+
+	public void setExpression(String expression) {
+		this.expression = expression;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getInterests() {
+		return interests;
+	}
+
+	public void setInterests(String interests) {
+		this.interests = interests;
+	}
+
+	public String getOccupancy() {
+		return occupancy;
+	}
+
+	public void setOccupancy(String occupancy) {
+		this.occupancy = occupancy;
+	}
+
+	public String getMobility() {
+		return mobility;
+	}
+
+	public void setMobility(String mobility) {
+		this.mobility = mobility;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
